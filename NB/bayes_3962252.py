390066cbc2a93cc69b142c476b938b12e54a44bb
Bryan Medina <bjmedina.cs@gmail.com>
2019-09-20 22:37:51 -0400

    TODO: Implement NaiveBayesClassifier class. Add functions to like indicator. Possibly a matrix for both the mus and sigmas (each feature for every training example should have one). Need to get the MLE estimates for mus and sigmas so that you can get the conditional probabilities.

---
+++ NB/bayes_3962252.py
@@ -0,0 +1,11 @@
+# Bryan Medina
+# CAP5610 - Machine Learning
+# Naive Bayes w/ Iris Dataset
+
+####### Imports ########
+import sys
+import time
+
+import numpy as np
+import pandas as pd
+########################
